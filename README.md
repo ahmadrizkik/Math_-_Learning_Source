243c2ccf018c69080f7b517325da1749158219e3
Ahmad Rizki Kurniawan <joshevans089@gmail.com>
2022-03-18 23:53:15 +0700

    update

---
+++ README.md
@@ -0,0 +1,2 @@
+# Math_-_Learning_Source
+My note through learning Math for Data Science
---
+++ test1.py
@@ -0,0 +1,7 @@
+# code for Sigma
+
+all_value = 0
+for value in range(4,26):
+    all_value = all_value + float(18 * value**3)
+
+print (all_value)
